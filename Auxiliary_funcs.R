54ce2cb0047bc5573368c802fafce5bfdb8977b7
Codicolus <ch.vonmatt@gmx.ch>
2019-07-08 16:07:11 +0200

    Web-Scraping of Temperature Information

--- Auxiliary_funcs.R
+++ Auxiliary_funcs.R
@@ -3,3 +3,82 @@
 # Created by: Christoph von Matt
 # Date: 08.07.2019
 # Licence: CC-BY-SA
+
+# BEACH-WEATHER-RELATED AUXILIARY FUNCTIONS
+############################################
+# Functions to calculate the contribution of temperatur, precipitation, sunshine, globalstrahlung, feuchtigkeit, wind
+# Temperature Contribution
+temp_cont <- function(value, wgth, max){
+  # if temperature is below zero then value set to zero as other factors are dominant
+  # then the variable is "standardized" according to meaningful values
+  # returned is the standardized value * weight
+  #min = 0
+  #max = 45
+  
+  if(value < 0){
+    value = 0
+  }
+  
+  std <- 1 / max * value
+  
+  return(std*wgth)
+}
+
+# Precipitation Contribution
+prec_cont <- function(value, wgth){
+  # Principle: contribution only when no rain!
+  if(value > 0){
+    return(0)
+  }
+  
+  return(wgth)
+}
+
+# Sunshine Contribution
+sun_cont <- function(value, wgth, max){
+  # min = 0
+  # max = 10
+  
+  std <- 1 / max * value
+  
+  return(std*wgth)
+}
+
+# Global Radiation Contribution
+glob_cont <- function(value, wgth, max){
+  #max = 1000
+  
+  std <- 1 / max * value
+  
+  return(std*wgth)
+}
+
+# Relative Humidity Contribution
+feu_cont <- function(value, wgth){
+  max = 100
+  
+  std <- 1 / max * value
+  
+  # for extremely high RH the weight is halved
+  if(value > 90){
+    wgth = wgth * 0.5
+  }
+  
+  return(std * wgth)
+}
+
+# Wind Speed Contribution
+wind_cont <- function(value, wgth, max){
+  # max acceptable wind speed = 25
+  # the higher the speed the less less it contributes to the index
+  # max = 25
+  
+  if(value > max){
+    value = max
+  }
+  
+  std <- 1 / max * value
+  
+  return((1-std)*wgth)
+}
+
--- preprocessing_riverdata.R
+++ preprocessing_riverdata.R
@@ -5,3 +5,127 @@
 # Date: 08.07.2019
 # Licence: CC-BY-SA
 
+# libraries
+library(httr)
+library(XML)
+library(rjson)
+library(stringr)
+
+###################################################################################################
+# Section 0: Auxiliary Functions
+extract_webURL <- function(attribute_containing_URL){
+  attribute2HTML <- htmlParse(attribute_containing_URL)
+  
+  # Returns the href-attribute of the given string
+  return(xpathApply(attribute2HTML, "//a/@href")[[1]][1])
+}
+
+# Reads the BAFU-HTML-table of measurements of a station site
+# Returns a table
+read_station_HTMLtable <- function(url){
+  
+  # Opening web-page
+  station_site <- GET(url)
+  station_site <- htmlParse(station_site, encoding = "UTF-8")
+  station_site <- xmlRoot(station_site)
+  
+  # Return table
+  return(readHTMLTable(station_site, which = 1, header = F))
+  
+}
+
+# Scrapes current temperature data from river-station
+scrape_temperature <- function(data_table){
+  
+  # Returns Letzer Messwert, Mittelwert über letzte 24h, Maximum letzte 24h
+  return(as.numeric(as.vector(data_table[1:3, "V4"])))
+}
+
+# Extracts values from string + turns it into numeric
+get_attributes_as_numeric <- function(string_with_value){
+  return(as.numeric(strsplit(string_with_value, " ")[[1]][1]))
+}
+
+
+# Scrapes meta-data from riverstation
+scrape_metadata <- function(data_table){
+  
+  # output
+  out <- rep(NA, 2)
+  
+  # Returns Stationshöhe, Grösse des Einzugsgebiets
+  values <- as.vector(data_table[6:7, "V2"])
+  
+  for (i in 1:2){
+    out[i] <-get_attributes_as_numeric(values[i])
+  }
+  return(out)
+}
+
+
+###################################################################################################
+# Section 1: Fetching newest river- data
+# TODO: scrape last measurement value of each river (if it doesn't take too long)
+# TODO: include river-temperatures in the interpolation of the badewetter-index too?
+# -->or just use the actual temperatures as additional conditon which the user could specify on its own
+# --> such that then the bade-index is alterd such that only regions are displayed where also the river/lake temperatures
+# are adequately for bathing
+
+# url
+file <- "http://data.geo.admin.ch/ch.bafu.hydroweb-messstationen_temperatur/ch.bafu.hydroweb-messstationen_temperatur_de.json"
+
+# read-in JSON-format
+rivers_json <- fromJSON(file=file)
+river_features <- rivers_json$features
+
+
+# Updated feature-list
+updated_features <- list()
+
+# Add Temperature and Metadata-Information for every feature in data-set
+# Loop over all features
+for (i in 1:length(river_features)){
+  
+  print(i)
+  
+  # get current feature
+  feature <- river_features[[i]]
+  
+  # get current properties
+  ft_props <- feature$properties
+  # Get Web-URL
+  ft_url <- extract_webURL(ft_props$description)
+  
+  # Get Temperature Values: 1 = current, 2 = mean 24h, 3 = max 24h
+  temperatures <- scrape_temperature(read_station_HTMLtable(ft_url))
+  # Get Metadata: 1 = Stationshöhe (m.ü.M.), 2 = Einzugsgebiet (km^2)
+  stat_meta <- scrape_metadata(read_station_HTMLtable(ft_url))
+  
+  # Add the additional elements to the feature properties
+  # Current Temperature
+  ft_props$current <- temperatures[1]
+  # Mittlere Temperature 24h
+  ft_props$mittlereT24h <- temperatures[2]
+  # Maximale Temperatur 24h
+  ft_props$maxT24h <- temperatures[3]
+  
+  # Stationshöhe
+  ft_props$statHeight <- stat_meta[1]
+  # Einzugsgebiet
+  ft_props$catchment <- stat_meta[2]
+  
+  
+  # Update features
+  feature$properties <- ft_props
+  
+  # add updated feature to new feature-list
+  updated_features[[i]] <- feature
+  
+}
+
+# Convert update list to JSON
+updated_features <- toJSON(updated_features)
+
+# TODO: could also be other data format
+# write Updated JSON
+write(updated_features, file="data/flussdaten_updated.json")
