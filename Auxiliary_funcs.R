eb15a8ce7bf0a78e0ca414862e7f6559eca77d9a
Codicolus <ch.vonmatt@gmx.ch>
2019-07-08 13:53:43 +0200

    Starting State
    Copied most important things from OpenData_codicolus repository

---
+++ Auxiliary_funcs.R
@@ -0,0 +1,5 @@
+# auxiliary functions for Badewetter-App / beach-weather app preprocessing
+# This script includes auxiliary functions used for the processing of the Badewetter-App Schweiz
+# Created by: Christoph von Matt
+# Date: 08.07.2019
+# Licence: CC-BY-SA
---
+++ preprocessing_riverdata.R
@@ -0,0 +1,7 @@
+# This script is for the preprocessing of river + lake measurements from the Federal Office of Environment
+# Preprocessing for the swiss beach-weather index (Badewetter-Index Schweiz)
+# The application can be found at: https://badewetter-index-schweiz.opendata.iwi.unibe.ch/
+# Created by: Christoph von Matt
+# Date: 08.07.2019
+# Licence: CC-BY-SA
+
